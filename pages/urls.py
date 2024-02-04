0cce71fc734df2cdf26aee1d79ace559bf10e72e
christopherece <christopheranchetaece@gmail.com>
2024-02-04 23:39:48 +1300

    Configure Contact Area

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
+    path('submitcontact', views.submitcontact, name='submitcontact'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from posts.models import Category, Post
+from .models import Contact
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.contrib import messages
+from django.utils import timezone
+
 
 # Create your views here.
 def index(request):
@@ -14,6 +18,33 @@ def index(request):
         'posts': paged_posts
     }
     return render(request, 'pages/index.html', context)
+
+def submitcontact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+    # Check if the email is already exist
+        # Check if the email and date combination already exists
+        today_start = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        today_end = today_start + timezone.timedelta(days=1)
+        if Contact.objects.filter(email=email, date__range=(today_start, today_end)).exists():
+            messages.error(request, 'You have already submitted a message with this email today. Please wait for response!')
+            return render(request, 'pages/contact.html')
+
+    contact = Contact(
+        name = name,
+        email = email, 
+        subject = subject,
+        message = message,
+
+    )
+
+    contact.save()
+    messages.success(request, 'Your message has been submitted')
+    return render(request, 'pages/contact.html')
 
 def about(request):
     return render(request, 'pages/about.html')
