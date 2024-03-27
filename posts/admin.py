300552e97ba03607ce2a771c94a111fdf308e7af
christopherece <christopheranchetaece@gmail.com>
2024-03-27 22:15:25 +1300

    Edit admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -5,7 +5,19 @@ from .models import Comment
 
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'content',
+        'author',
+        'is_published',
+        'created_at',
+        'updated_at',
+    )
+    list_editable = ['is_published',]
+
+
 
 admin.site.register(Comment)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
